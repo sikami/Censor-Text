854bb8c8f7c6998b3a53b623c5f004d8031925e5
sikami <widyasari.listya@gmail.com>
2020-09-03 14:29:32 +0100

    added comment and changed file path, so user can add their own path

--- Censor.java
+++ Censor.java
@@ -10,21 +10,27 @@ public class Censor {
     private Dictionary dictionary;
     private File file;
 
-    public Censor(File file) {
-        this.dictionary = new Dictionary(new File("/home/listya/Documents/Belajar/Java/REPROMPT/src/com/com/censor/banned.txt"));
-        this.file = file;
+    public Censor(File textPath, String dictionaryPath) {
+        this.dictionary = new Dictionary(new File(dictionaryPath));
+        this.file = textPath;
     }
 
 
     public void censorFile() throws IOException {
-
+    // get the Hashset from class Dictionary where the censored words been uploaded
         HashSet<String> dict = dictionary.getDict();
-        FileWriter output = new FileWriter("newfile.txt");
+
+    //create file output path
+        FileWriter output = new FileWriter(System.getProperty("user.dir") + "/outputText.txt");
+
+    //create variable to store the text words
         String x, newWord;
         StringBuilder sb = new StringBuilder();
+
+    //read text file word per word and match it to Hashset from class dictionary, if found, replace bad words with *
         try(Scanner scanner = new Scanner(new FileReader(file))) {
             while(scanner.hasNextLine()) {
-                x = scanner.nextLine();
+                x = scanner.nextLine().toLowerCase();
                 String[] words = x.split(" ");
 
                 for(String word : words) {
@@ -34,7 +40,6 @@ public class Censor {
                             wordss += '*';
                         }
                         newWord = word.replace(word, wordss);
-                        //output.write(newWord);
                         sb.append(newWord + " ");
 
                     } else {
@@ -45,7 +50,10 @@ public class Censor {
         } catch (FileNotFoundException e) {
             e.getMessage();
         }
-        System.out.println(sb);
+
+    //write the output to the output text file and close the filewriter
+        output.write(sb.toString());
+        output.close();
 
     }
 }
--- Main.java
+++ Main.java
@@ -5,7 +5,8 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Censor censor = new Censor(new File("/home/listya/Documents/Belajar/Java/REPROMPT/src/com/com/censor/sample.txt"));
+        Censor censor = new Censor(new File("Put Your Text File Path here (the one to be censored)"),
+                "If you have dictionary of bad words, put the dictionary path here");
         censor.censorFile();
     }
 }
